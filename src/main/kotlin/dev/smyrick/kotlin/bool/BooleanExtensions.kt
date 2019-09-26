056d3a2f4133a62e981b40f91e336810eb86af94
smyrick <smyrick@expedia.com>
2019-09-25 17:32:52 -0700

    Update publish settings

--- src/main/kotlin/dev/smyrick/kotlin/bool/BooleanExtensions.kt
+++ src/main/kotlin/dev/smyrick/kotlin/bool/BooleanExtensions.kt
@@ -1,3 +1,10 @@
 package dev.smyrick.kotlin.bool
 
+/**
+ * Converts a nullable Boolean into non-null
+ *
+ *  - true -> true
+ *  - false -> false
+ *  - null -> false
+ */
 fun Boolean?.toNonNull() = this == true
