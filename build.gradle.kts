25ee23803172a62bc10c85c3880115a409796694
Shane Myrick <mail@shanemyrick.com>
2019-09-25 17:35:32 -0700

    Update publish settings (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.jvm.tasks.Jar
 
 plugins {
     kotlin("jvm") version "1.3.50"
+    id("org.jetbrains.dokka") version "0.9.17"
+    `maven-publish`
 }
 
 group = "dev.smyrick"
@@ -23,4 +26,31 @@ tasks.withType<KotlinCompile> {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(tasks.dokka)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+            artifact(dokkaJar)
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("$buildDir/repository")
+        }
+    }
 }
